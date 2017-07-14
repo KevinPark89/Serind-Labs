f442740dd283a8e8c4b0ad97895b1e4b73c8bcc2
Kevin Park <kevin.park0989@gmail.com>
2017-07-14 10:36:29 -0400

    fixed error

--- src/component/StarwarsItem.js
+++ src/component/StarwarsItem.js
@@ -2,14 +2,11 @@ import React, { Component } from 'react';
 
 class StarwarsItem extends Component {
     render() {
-       let properties = [];
-       for(let key in this.props.selectedItem) {
-                  properties.push(
-            <li key={this.props.selectedItem.url+this.props.selectedItem.name}>
-              {this.props.selectedItem[key]}
-          </li>
-         );
-             }
+       let properties = Object.keys(this.props.selectedItem).map((key, i) => {
+           return (
+            <li className="itemList" key={i}>{key}: {this.props.selectedItem[key]}</li>
+        )
+       })
        return (
         <div>
             <ul>{properties}</ul>
